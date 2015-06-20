a03a60871e2d8967436a3f9937f914cc42f11795
Víctor Manuel González Souto <glezsoutovm@hotmail.com>
2015-06-20 10:37:38 +0200

    Autoload de la base de datos.

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Página de créditos
 router.get('/author', function(req, res) {
   res.render('author');
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+// Definición de rutas de /quizes
 router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
