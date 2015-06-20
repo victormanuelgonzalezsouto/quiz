731d55f2287fc5052fc3284cfa0199fc26df2e09
Víctor Manuel González Souto <glezsoutovm@hotmail.com>
2015-06-20 17:16:53 +0200

    Borrar preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -97,3 +97,12 @@ exports.update = function(req, res) {
 		});
 	}
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then(function() {
+		res.redirect('/quizes');
+	}).catch(function(error) {
+		next(error);
+	});
+};
